47fbdd380db44be109325ec026817e1df0295a3f
Sai Kumar <saikumarpasupuleti497@gmail.com>
2026-05-19 23:46:03 -0500

    Initial Commit of the project

---
+++ src/pages/Home.js
@@ -0,0 +1,52 @@
+
+import supabase from "../config/SuperbaseClient"
+import { useEffect, useState } from "react"
+
+// Components
+import SmoothieCart from "../components/SmoothieCart"
+
+
+
+const Home = () => {
+  
+  const [FetchError, setFetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from('Smoothies')
+        .select('*')
+  
+      if (error) {
+        setFetchError('Could not fetch smoothies')
+        setSmoothies(null)
+        console.log(error)
+      }
+
+      if (data) {
+        setSmoothies(data)
+        setFetchError(null)
+      }
+    }
+
+    fetchSmoothies()
+  }, [])
+
+  return (
+    <div className="page home">
+      {FetchError && <p>{FetchError}</p>}
+      {smoothies && (
+        <div className = "smoothies">
+          <div className="smoothie-grid">
+            {smoothies.map(smoothie => (
+            <SmoothieCart key={smoothie.id} smoothie={smoothie} />
+          ))}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Home
